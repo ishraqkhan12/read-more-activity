df37e79762bd1ebe8676bdf18ed55276d7a5cbb2
ishraqkhan12 <157279448+ishraqkhan12@users.noreply.github.com>
2024-08-17 02:45:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let e = ' This is more text'
+
+
+function readMore(){
+let p = document.querySelector("p")
+ p.innerText += e;
+const linktag = document.querySelector('#link')
+linktag.innerHTML = '' 
+//  linktag.innerText = "read less"
+ 
+
+
+}
+
+
